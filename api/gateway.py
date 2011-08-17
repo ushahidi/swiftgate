92913dbd5727deb024e5671c3122e0616acee231
Charl van Niekerk <charlvn@charlvn.com>
2011-08-17 14:03:09 +0300

    Added controller

---
+++ api/gateway.py
@@ -0,0 +1,87 @@
+# SwiftRiver Gateway Controller
+# =============================
+#
+# This file is part of SwiftRiver Gateway.
+#
+# SwiftRiver Gateway is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SwiftRiver Gateway is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with SwiftRiver Gateway.  If not, see <http://www.gnu.org/licenses/>.
+
+from flask import abort, Flask, make_response, request
+from inspect import getargspec
+from json import dumps
+from pymongo import Connection
+from validator import Validator
+
+app = Flask(__name__)
+
+@app.route('/api/<method_name>', methods=['GET', 'POST'])
+def api(method_name):
+    db = Connection().riverid
+    api = API(db)
+    method = getattr(api, method_name, False)
+
+    if method == False:
+        abort(404)
+
+    method_parameters = getargspec(method).args
+    method_parameters.remove('self')
+
+    if request.method == 'POST':
+        request_parameters = request.form.to_dict()
+    else:
+        request_parameters = request.args.to_dict()
+
+    if 'callback' in request_parameters:
+        callback = request_parameters['callback']
+        Validator.callback(callback)
+    else:
+        callback = False
+
+    for key in method_parameters:
+        if key not in request_parameters:
+            abort(400)
+    
+    unused_parameters = []
+    for key in request_parameters:
+        if key not in method_parameters:
+            unused_parameters.append(key)
+    for key in unused_parameters:
+        del request_parameters[key]
+
+    result = dict(method=method_name, request=request_parameters)
+
+    try:
+        result['response'] = method(**request_parameters)
+        result['success'] = True
+    except RiverException as (error,):
+        result['success'] = False
+        result['error'] = error
+
+    json = dumps(result)
+
+    if callback:
+        javascript = '%s(%s);\n' % (callback, json)
+        response = make_response(javascript)
+        response.headers['Content-Type'] = 'application/javascript; charset=UTF-8'
+    else:
+        response = make_response(json + '\n')
+        response.headers['Content-Type'] = 'application/json; charset=UTF-8'
+
+    return response
+
+def main():
+    app.debug = True
+    app.run(host='0.0.0.0')
+
+if __name__ == '__main__':
+    main()
