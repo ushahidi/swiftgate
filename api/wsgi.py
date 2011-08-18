973081a282d863ab0215939a784397e1b400a614
Charl van Niekerk <charlvn@charlvn.com>
2011-08-18 14:41:16 +0300

    Renamed to SwiftGate

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,20 +1,20 @@
-# SwiftRiver Gateway WSGI Handler
+# SwiftGate WSGI Handler
 # ===============================
 #
-# This file is part of SwiftRiver Gateway.
+# This file is part of SwiftGate.
 #
-# SwiftRiver Gateway is free software: you can redistribute it and/or modify
+# SwiftGate is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# SwiftRiver Gateway is distributed in the hope that it will be useful,
+# SwiftGate is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with SwiftRiver Gateway.  If not, see <http://www.gnu.org/licenses/>.
+# along with SwiftGate.  If not, see <http://www.gnu.org/licenses/>.
 
 import os, sys
 sys.path.append(os.path.dirname(__file__))
