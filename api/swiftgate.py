af017d3d326c903d67e066c38a5cbb108c3f3775
Charl van Niekerk <charlvn@charlvn.com>
2011-08-30 14:13:12 +0300

    Added scribe/thrift

--- api/swiftgate.py
+++ api/swiftgate.py
@@ -18,9 +18,23 @@
 
 from flask import abort, Flask, make_response, request
 from httplib import HTTPConnection
+from scribe import scribe
+from thrift.transport import TTransport, TSocket
+from thrift.protocol import TBinaryProtocol
 import json, pika
  
 app = Flask(__name__)
+
+#category='test'
+#message='hello world'
+#log_entry = scribe.LogEntry(category, message)
+#socket = TSocket.TSocket(host='localhost', port=1463)
+#transport = TTransport.TFramedTransport(socket)
+#protocol = TBinaryProtocol.TBinaryProtocol(trans=transport, strictRead=False, strictWrite=False)
+#client = scribe.Client(iprot=protocol, oprot=protocol)
+#transport.open()
+#result = client.Log(messages=[log_entry])
+#transport.close()
 
 pika_parameters = pika.ConnectionParameters('localhost')
 pika_connection = pika.BlockingConnection(pika_parameters)
