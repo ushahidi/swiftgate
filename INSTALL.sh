91f9d8227af7198e69da220b9e9527cc3c925329
Charl van Niekerk <charlvn@charlvn.com>
2011-09-01 13:36:58 +0300

    Remove fullstops off comments.

--- INSTALL.sh
+++ INSTALL.sh
@@ -18,40 +18,40 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with SwiftGate.  If not, see <http://www.gnu.org/licenses/>.
 
-# Add the RabbitMQ public key to the trusted key list.
+# Add the RabbitMQ public key to the trusted key list
 wget -qO- http://www.rabbitmq.com/rabbitmq-signing-key-public.asc | apt-key add -
 	 	
-# Add the RabbitMQ repository to the sources.
+# Add the RabbitMQ repository to the sources
 echo deb http://www.rabbitmq.com/debian/ testing main >> /etc/apt/sources.list
 
-# Update the sources.
+# Update the sources
 apt-get update
 
-# Upgrade the existing packages.
+# Upgrade the existing packages
 apt-get upgrade -y
 
-# Install the other necessary Debian packages.
+# Install the other necessary Debian packages
 apt-get install -y apache2 libapache2-mod-wsgi python-pip rabbitmq-server git libboost-dev libevent-dev python-dev automake pkg-config libtool flex bison ant openjdk-6-jdk bjam libboost-all-dev build-essential psmisc
 
-# Install the necessary Python packages.
+# Install the necessary Python packages
 pip install flask python-memcached pika
 
-# Create a user for SwiftGate processes to run as.
+# Create a user for SwiftGate processes to run as
 adduser --disabled-password --gecos "" swiftgate
 
 # Create a local clone of the application.
 git clone https://github.com/ushahidi/swiftgate.git /var/www/swiftgate
 
-# Remove the default Apache configuration.
+# Remove the default Apache configuration
 rm /etc/apache2/sites-enabled/000-default
 
-# Copy the SwiftGate Apache configuration.
+# Copy the SwiftGate Apache configuration
 cp /var/www/swiftgate/config/apache.conf /etc/apache2/sites-enabled/swiftgate.conf
 
-# Tell Apache to reload its configuration.
+# Tell Apache to reload its configuration
 /etc/init.d/apache2 reload
 
-# Install RabbitMQ management plugins.
+# Install RabbitMQ management plugins
 wget -O /tmp/mochiweb-1.3-rmq2.6.0-git9a53dbd.ez http://www.rabbitmq.com/releases/plugins/v2.6.0/mochiweb-1.3-rmq2.6.0-git9a53dbd.ez
 mv /tmp/mochiweb-1.3-rmq2.6.0-git9a53dbd.ez /usr/lib/rabbitmq/lib/rabbitmq_server-*/plugins/
 wget -O /tmp/webmachine-1.7.0-rmq2.6.0-hg0c4b60a.ez http://www.rabbitmq.com/releases/plugins/v2.6.0/webmachine-1.7.0-rmq2.6.0-hg0c4b60a.ez
@@ -65,7 +65,7 @@ mv /tmp/rabbitmq_management_agent-2.6.0.ez /usr/lib/rabbitmq/lib/rabbitmq_server
 wget -O /tmp/rabbitmq_management-2.6.0.ez http://www.rabbitmq.com/releases/plugins/v2.6.0/rabbitmq_management-2.6.0.ez
 mv /tmp/rabbitmq_management-2.6.0.ez /usr/lib/rabbitmq/lib/rabbitmq_server-*/plugins/
 
-# Create RabbitMQ administrative user.
+# Create RabbitMQ administrative user
 rabbitmqctl add_user swiftgate swiftgate
 rabbitmqctl set_user_tags swiftgate administrator
 
@@ -115,11 +115,11 @@ rm -rf /tmp/scribe
 # Update Libraries
 ldconfig
 
-# Download Membase.
+# Download Membase
 wget -O /tmp/membase-server-community_x86_64_1.7.1.deb http://packages.couchbase.com/releases/1.7.1/membase-server-community_x86_64_1.7.1.deb
 
-# Install Membase.
+# Install Membase
 dpkg -i /tmp/membase-server-community_x86_64_1.7.1.deb
 
-# Remove the Membase installer.
+# Remove the Membase installer
 rm -f /tmp/membase-server-community_x86_64_1.7.1.deb
